92f13c60f1708c13b1c890b7012893953e568555
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-07 13:18:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "16277846"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "47c0957eddc91f492f0ac6da481197af"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6065525851"
+SUPPORT_GRP = "lafze_ruh"
+UPDATE_CHNL = "Dpz_stylish_world"
+OWNER_USERNAME = "shayarakhawab"
 
